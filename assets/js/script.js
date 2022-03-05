d24039f9037b25eb535017651b22e2d6e7fe4516
gpay <gpaytakov@gmail.com>
2022-03-05 12:00:26 -0600

    question feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,66 +23,20 @@ var questionBank = [
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
-var introInfo = document.querySelector(".start-quiz");
-var questionPrompt = document.querySelector(".question-prompt");
-var choice1 = document.getElementById("c-1");
+var introInfo = document.querySelector('.start-quiz');
+var questionPrompt = document.querySelector('.question-prompt');
+var choice1 = document.getElementById('c-1');
 var choice2 = document.getElementById('c-2');
 var choice3 = document.getElementById('c-3');
 var choice4 = document.getElementById('c-4');
-var initialsInput = document.querySelector("#initials");
-var resultsButton = document.querySelector("#results");
-var olEl = document.getElementById("answers-to-choose");
-
+var initialsInput = document.querySelector('#initials');
+var resultsButton = document.querySelector('#results');
+var olEl = document.getElementById('answers-to-choose');
+var feedback = document.querySelector('.feedback');
+var choiceFeedback = document.getElementById('question-feedback')
 var timeLeft = 60;
 var quizTimer;
 var runTimer = false;
-function startTimer() {
-    // display time
-    if (runTimer) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft;
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(quizTimer);
-        // execute a function to end the game
-        showResult();
-      }
-}
-
-// currentQuestionIndex = cQI
-var cQI = 0;
-
-function getQuestion() {
-    const el = document.querySelector("#quiz-quest");
-    console.log(el);
-        if (timeLeft === 0 || cQI >= questionBank.length) {
-            runTimer = false;
-            el.classList.add("hide")
-            showResult();
-        } else {
-            el.classList.remove("hide")
-            questionPrompt.innerHTML = questionBank[cQI].question;
-            choice1.innerHTML = questionBank[cQI].choices[0][0];
-            choice2.innerHTML = questionBank[cQI].choices[1][0];
-            choice3.innerHTML = questionBank[cQI].choices[2][0];
-            choice4.innerHTML = questionBank[cQI].choices[3][0];
-        } 
-}
-// resultsButton.addEventListener("click", function(event) {
-//     event.preventDefault();
-
-//     var quizTaker = {
-//         initials: initialsInput.value.trim()
-//     }
-// });
-
-function showResult() {
-    console.log("Show Result");   
-}
 
 // create a function to start a quiz
 function startQuiz() {
@@ -96,18 +50,80 @@ function startQuiz() {
     getQuestion();
 }
 
+function startTimer() {
+    // display time
+    if (runTimer) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(quizTimer);
+        // execute a function to end the game
+      }
+}
+
+// currentQuestionIndex = cQI
+var cQI = 0;
+
+function getQuestion() {
+    const el = document.querySelector("#quiz-quest");
+    console.log(el);
+        if (timeLeft === 0 || cQI >= questionBank.length) {
+            runTimer = false;
+            el.classList.add("hide")
+            showResult(timeLeft);
+        } else {
+            el.classList.remove("hide")
+            questionPrompt.innerHTML = questionBank[cQI].question;
+            choice1.innerHTML = questionBank[cQI].choices[0][0];
+            choice2.innerHTML = questionBank[cQI].choices[1][0];
+            choice3.innerHTML = questionBank[cQI].choices[2][0];
+            choice4.innerHTML = questionBank[cQI].choices[3][0];
+        } 
+}
+resultsButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var quizTaker = {
+        initials: initialsInput.value.trim()
+    }
+});
+
+function showResult(score) {
+    console.log(score);   
+};
+
+
+
+//show question feedback
+function questionFeedback(tf) {
+    choiceFeedback.classList.remove("hide")
+    if (tf) {
+        feedback.innerHTML = "Correct!"
+    }
+    else {
+        feedback.innerHTML = "Wrong!"
+    } 
+    setTimeout(function() {
+        choiceFeedback.classList.add("hide")
+    }, 1000);   
+}
+    
 function olAnswerClick(event) {
     var index = parseInt(event.target.id.split("-")[1])-1;
-    console.log(questionBank[cQI].choices[index][1]);
     var tf = questionBank[cQI].choices[index][1];
+    console.log(tf)
     cQI++;
         if (!tf) {
         timeLeft = timeLeft-10;
         timerEl.textContent = timeLeft;
     }
     getQuestion()
-    //displayCorrectOrWrong();
-    //setTimeout(remove correct or wrong, 500);
+    questionFeedback(tf);
 }
 
 startBtn.addEventListener("click", startQuiz);
